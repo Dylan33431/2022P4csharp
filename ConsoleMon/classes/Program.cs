5692ebca891637982e5059d5d73d05fff378290e
Dylan33431 <89254356+Dylan33431@users.noreply.github.com>
2022-06-13 20:50:22 +0200

    constructors

--- ConsoleMon/classes/Program.cs
+++ ConsoleMon/classes/Program.cs
@@ -18,6 +18,7 @@ namespace ConsoleMon
         string name = "Xuritree";
         int Health = 70;
         int Energy = 60;
+        int damage;
         Element weakness = Element.water;
 
         public void TakeDamage(int Damage)
@@ -31,5 +32,27 @@ namespace ConsoleMon
         }
 
         List<Skill> skills = new List<Skill>();
+
+        internal ConsoleMon()
+        {
+
+        }
+
+        internal ConsoleMon(ConsoleMon copyFrom)
+        {
+            this.name = copyFrom.name;
+            this.Health = copyFrom.Health;
+            this.Energy = copyFrom.Energy;
+
+            this.damage = copyFrom.damage;
+
+            for (int i = 0; i < copyFrom.skills.Count; i++)
+            {
+                Skill copyFromThisSkill = copyFrom.skills[i];
+                Skill copy = new Skill(copyFromThisSkill);
+
+                this.skills.Add(copy);
+            }
+        }
     }
 }
--- ConsoleMon/classes/Skills.cs
+++ ConsoleMon/classes/Skills.cs
@@ -13,10 +13,23 @@ namespace ConsoleMon
         float damage = 120;
         float energyCost = 60;
         Element element = Element.electro;
+
         public void UseOn(ConsoleMon target, ConsoleMon caster)
         {
             target.TakeDamage(120);
             caster.DepleteEnergy(60);
+        }
+
+        internal Skill()
+        {
+
+        }
+
+        internal Skill(Skill copyFrom)
+        {
+            this.damage = copyFrom.damage;
+            this.energyCost = copyFrom.energyCost;
+            this.name = copyFrom.name;
         }
 
     }
