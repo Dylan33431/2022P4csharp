157e2304d91c3a373da98b63247c60c8fdf9a57d
Dylan33431 <89254356+Dylan33431@users.noreply.github.com>
2022-06-13 19:06:16 +0200

    function opdracht

--- ConsoleMon/classes/Program.cs
+++ ConsoleMon/classes/Program.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace classes
+namespace ConsoleMon
 {
-    internal class enemy
-    {
-        private string name;
-        private int hp;
-        private int energy;
-    }
 
-    internal class skill
+    internal class ConsoleMon
     {
-        private string skillname;
-        private int dmg;
-        private int energycost;
+        string name = "Xuritree";
+        int Health = 70;
+        int Energy = 60;
+
+
+        public void TakeDamage(int Damage)
+        {
+            Health = -Damage;
+        }
+
+        public void DepleteEnergy(int energy)
+        {
+            Energy = -energy;
+        }
+
+        List<Skill> skills = new List<Skill>();
     }
 }
---
+++ ConsoleMon/classes/Skills.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        string name = "Zap Cannon";
+        float damage = 120;
+        float energyCost = 60;
+
+        public void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            target.TakeDamage(120);
+            caster.DepleteEnergy(60);
+        }
+
+    }
+
+}
